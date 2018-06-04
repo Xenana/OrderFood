0581d3891c4414e5f62e62a58892fce1c6b457c5
Xenana <k-shabatko@mail.ru>
2018-06-04 16:55:50 +0400

    Лабораторная 10

--- AbstractOrderFood/AbstractOrderFoodService/ImplementationsList/OrderFoodServiceList.cs
+++ AbstractOrderFood/AbstractOrderFoodService/ImplementationsList/OrderFoodServiceList.cs
@@ -21,69 +21,32 @@ namespace AbstractOrderFoodService.ImplementationsList
 
         public List<BasketCourseViewModel> GetList()
         {
-            List<BasketCourseViewModel> result = new List<BasketCourseViewModel>();
-            for (int i = 0; i < source.Baskets.Count; ++i)
-            {
-                string customerFIO = string.Empty;
-                for(int j=0; j<source.Customer.Count; ++j)
+            List<BasketCourseViewModel> result = source.Baskets
+                .Select(rec => new BasketCourseViewModel
                 {
-                    if(source.Customer[j].Id == source.Baskets[i].CustomersId)
-                    {
-                        customerFIO = source.Customer[j].CustomersFIO;
-                        break;
-                    }
-                }
-                string courseSetName = string.Empty;
-                for(int j=0; j<source.CourseSet.Count; ++j)
-                {
-                    if(source.CourseSet[j].Id == source.Baskets[i].CourseSetsId)
-                    {
-                        courseSetName = source.CourseSet[j].CourseSetsName;
-                        break;
-                    }
-                }
-                string chefFIO = string.Empty;
-                if (source.Baskets[i].ChefsId.HasValue)
-                {
-                    for(int j=0; j<source.Chef.Count; ++j)
-                    {
-                        if(source.Chef[j].Id == source.Baskets[i].ChefsId.Value)
-                        {
-                            chefFIO = source.Chef[j].ChefsFIO;
-                            break;
-                        }
-                    }
-                }
-                result.Add(new BasketCourseViewModel
-                {
-                    Id = source.Baskets[i].Id,
-                    CustomersId = source.Baskets[i].CustomersId,
-                    CustomersFIO = customerFIO,
-                    CourseSetsId = source.Baskets[i].CourseSetsId,
-                    CourseSetsName = courseSetName,
-                    ChefsId = source.Baskets[i].ChefsId,
-                    ChefsName = chefFIO,
-                    Count = source.Baskets[i].Count,
-                    Sum = source.Baskets[i].Sum,
-                    DateCreate = source.Baskets[i].DateCreate.ToLongDateString(),
-                    DateImplement = source.Baskets[i].DateImplement?.ToLongDateString(),
-                    Status = source.Baskets[i].Status.ToString()
-                });
-            }
-
+                    Id = rec.Id,
+                    CustomersId = rec.CustomersId,
+                    CourseSetsId = rec.CourseSetsId,
+                    ChefsId = rec.ChefsId,
+                    Count = rec.Count,
+                    Sum = rec.Sum,
+                    DateCreate = rec.DateCreate.ToLongDateString(),
+                    DateImplement = rec.DateImplement?.ToLongDateString(),
+                    Status = rec.Status.ToString(),
+                    CustomersFIO = source.Customer
+                                    .FirstOrDefault(recCu => recCu.Id == rec.CustomersId)?.CustomersFIO,
+                    CourseSetsName = source.CourseSet
+                                    .FirstOrDefault(recCS => recCS.Id == rec.CourseSetsId)?.CourseSetsName,
+                    ChefsName = source.Chef
+                                    .FirstOrDefault(recCh => recCh.Id == rec.ChefsId)?.ChefsFIO
+                })
+                .ToList();
             return result;
         }
 
         public void CreateOrder(BasketCourseBindingModel model)
         {
-            int maxId = 0;
-            for(int i=0; i<source.Baskets.Count; ++i)
-            {
-                if (source.Baskets[i].Id > maxId)
-                {
-                    maxId = source.Customer[i].Id;
-                }
-            }
+            int maxId = source.Baskets.Count > 0 ? source.Baskets.Max(rec => rec.Id) : 0;
             source.Baskets.Add(new BasketCourse
             {
                 Id = maxId + 1,
@@ -98,134 +61,92 @@ namespace AbstractOrderFoodService.ImplementationsList
 
        public void TakeOrderInWork(BasketCourseBindingModel model)
         {
-            int index = -1;
-            for (int i = 0; i < source.Baskets.Count; ++i)
-            {
-                if (source.Baskets[i].Id == model.Id)
-                {
-                    index = i;
-                    break;
-                }
-            }
-            if (index == -1)
+            BasketCourse element = source.Baskets.FirstOrDefault(rec => rec.Id == model.Id);
+            if (element == null)
             {
                 throw new Exception("Элемент не найден");
             }
             // смотрим по количеству компонентов на складах
-            for (int i = 0; i < source.CourseSetsCourse.Count; ++i)
+            var courseSetsCourses = source.CourseSetsCourse.Where(rec => rec.CourseSetsId == element.CourseSetsId);
+            foreach (var courseSetsCourse in courseSetsCourses)
             {
-                if (source.CourseSetsCourse[i].CourseSetsId == source.Baskets[index].CourseSetsId)
+                int countOnKitchens = source.KitchenCourse
+                                            .Where(rec => rec.CoursesId == courseSetsCourse.CoursesId)
+                                            .Sum(rec => rec.Count);
+                if (countOnKitchens < courseSetsCourse.Count * element.Count)
                 {
-                    int countOnKitchens= 0;
-                    for (int j = 0; j < source.KitchenCourse.Count; ++j)
-                    {
-                        if (source.KitchenCourse[j].CoursesId == source.CourseSetsCourse[i].CoursesId)
-                        {
-                            countOnKitchens += source.KitchenCourse[j].Count;
-                        }
-                    }
-                    if (countOnKitchens < source.CourseSetsCourse[i].Count * source.Baskets[index].Count)
-                    {
-                        for (int j = 0; j < source.Course.Count; ++j)
-                        {
-                            if (source.Course[j].Id == source.CourseSetsCourse[i].CoursesId)
-                            {
-                                throw new Exception("Не достаточно блюда " + source.Course[j].CoursesName +
-                                    " требуется " + source.CourseSetsCourse[i].Count + ", в наличии " + countOnKitchens);
-                            }
-                        }
-                    }
+                    var courseName = source.Course
+                                    .FirstOrDefault(rec => rec.Id == courseSetsCourse.CoursesId);
+                    throw new Exception("Не достаточно блюда " + courseName?.CoursesName +
+                    " требуется " + courseSetsCourse.Count + ", в наличии " + countOnKitchens);
                 }
             }
             // списываем
-            for (int i = 0; i < source.CourseSetsCourse.Count; ++i)
+            foreach (var courseSetsCourse in courseSetsCourses)
             {
-                if (source.CourseSetsCourse[i].CourseSetsId == source.Baskets[index].CourseSetsId)
+                int countOnKitchens = courseSetsCourse.Count * element.Count;
+                var kitchenCourses = source.KitchenCourse
+                                            .Where(rec => rec.CoursesId == courseSetsCourse.CoursesId);
+                foreach (var kitchenCourse in kitchenCourses)
                 {
-                    int countOnKitchens = source.CourseSetsCourse[i].Count * source.Baskets[index].Count;
-                    for (int j = 0; j < source.KitchenCourse.Count; ++j)
-                    {
-                        if (source.KitchenCourse[j].CoursesId == source.CourseSetsCourse[i].CoursesId)
+                    // компонентов на одном слкаде может не хватать
+                    if (kitchenCourse.Count >= countOnKitchens)
                         {
-                            // компонентов на одном слкаде может не хватать
-                            if (source.KitchenCourse[j].Count >= countOnKitchens)
-                            {
-                                source.KitchenCourse[j].Count -= countOnKitchens;
-                                break;
-                            }
-                            else
-                            {
-                                countOnKitchens -= source.KitchenCourse[j].Count;
-                                source.KitchenCourse[j].Count = 0;
-                            }
+                            kitchenCourse.Count -= countOnKitchens;
+                            break;
                         }
+                    else
+                    {
+                        countOnKitchens -= kitchenCourse.Count;
+                        kitchenCourse.Count = 0;
                     }
                 }
             }
-            source.Baskets[index].ChefsId = model.ChefsId;
-            source.Baskets[index].DateImplement = DateTime.Now;
-            source.Baskets[index].Status = BasketCourseStatus.Выполняется;
+            element.ChefsId = model.ChefsId;
+            element.DateImplement = DateTime.Now;
+            element.Status = BasketCourseStatus.Выполняется;
         }
 
         public void FinishOrder(int id)
         {
-            int index = -1;
-            for (int i = 0; i < source.Baskets.Count; ++i)
-            {
-                if (source.Customer[i].Id == id)
-                {
-                    index = i;
-                    break;
-                }
-            }
-            if (index == -1)
+            BasketCourse element = source.Baskets.FirstOrDefault(rec => rec.Id == id);
+            if (element == null)
             {
                 throw new Exception("Элемент не найден");
             }
-            source.Baskets[index].Status = BasketCourseStatus.Готов;
+            element.Status = BasketCourseStatus.Готов;
         }
 
         public void PayOrder(int id)
         {
-            int index = -1;
-            for (int i = 0; i < source.Baskets.Count; ++i)
-            {
-                if (source.Customer[i].Id == id)
-                {
-                    index = i;
-                    break;
-                }
-            }
-            if (index == -1)
+            BasketCourse element = source.Baskets.FirstOrDefault(rec => rec.Id == id);
+            if (element == null)
             {
                 throw new Exception("Элемент не найден");
             }
-            source.Baskets[index].Status = BasketCourseStatus.Оплачен;
+            element.Status = BasketCourseStatus.Оплачен;
         }
 
         public void PutCoursesOnKitchen(KitchenCoursesBindingModel model)
         {
-            int maxId = 0;
-            for (int i = 0; i < source.KitchenCourse.Count; ++i)
+            KitchenCourses element = source.KitchenCourse
+                                                .FirstOrDefault(rec => rec.KitchenId == model.KitchenId &&
+                                                                    rec.CoursesId == model.CoursesId);
+            if (element != null)
             {
-                if (source.KitchenCourse[i].KitchenId == model.KitchenId &&
-                    source.KitchenCourse[i].KitchenId == model.KitchenId)
-                {
-                    source.KitchenCourse[i].Count += model.Count;
-                    return;
-                }
-                if (source.KitchenCourse[i].Id > maxId)
-                {
-                    maxId = source.KitchenCourse[i].Id;
-                }
+                element.Count += model.Count;
             }
-            source.KitchenCourse.Add(new KitchenCourses
+            else
             {
-                Id = ++maxId,
-                KitchenId = model.KitchenId,
-                CoursesId = model.CoursesId,
-                Count = model.Count
-            });
+                int maxId = source.KitchenCourse.Count > 0 ? source.KitchenCourse.Max(rec => rec.Id) : 0;
+                source.KitchenCourse.Add(new KitchenCourses
+                {
+                    Id = ++maxId,
+                    KitchenId = model.KitchenId,
+                    CoursesId = model.CoursesId,
+                    Count = model.Count
+                });
+            }
         }
     }
 }
