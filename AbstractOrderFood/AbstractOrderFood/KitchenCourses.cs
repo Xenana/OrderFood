0a81c9e983d6cf0240297f0e304d1b24280b110e
Xenana <k-shabatko@mail.ru>
2018-06-04 17:04:37 +0400

    Лабораторная 11

--- AbstractOrderFood/AbstractOrderFood/KitchenCourses.cs
+++ AbstractOrderFood/AbstractOrderFood/KitchenCourses.cs
@@ -15,5 +15,9 @@ namespace AbstractOrderFood
         public int CoursesId { get; set; }
 
         public int Count { get; set; }
+
+        public virtual Kitchen Kitchen { get; set; }
+
+        public virtual Courses Courses { get; set; }
     }
 }
