01d8244f3691b1fc25606e8ca1fa100b5331bb34
Xenana <k-shabatko@mail.ru>
2018-06-22 22:39:41 +0400

    Лабораторная 11

---
+++ AbstractOrderFood/AbstractOrderFoodService/AbstractDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractOrderFood;
+
+namespace AbstractOrderFoodService
+{
+    public class AbstractDbContext : DbContext
+    {
+        public AbstractDbContext() : base("AbstractDatabase")
+        {
+            //настройки конфигурации для entity
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+
+        public virtual DbSet<Customers> Customer { get; set; }
+
+        public virtual DbSet<Courses> Course { get; set; }
+
+        public virtual DbSet<Chefs> Chef { get; set; }
+
+        public virtual DbSet<BasketCourse> Baskets { get; set; }
+
+        public virtual DbSet<CourseSets> CourseSet { get; set; }
+
+        public virtual DbSet<CourseSetsCourses> CourseSetsCourse { get; set; }
+
+        public virtual DbSet<Kitchen> Kitchens { get; set; }
+
+        public virtual DbSet<KitchenCourses> KitchenCourse { get; set; }
+    }
+}
