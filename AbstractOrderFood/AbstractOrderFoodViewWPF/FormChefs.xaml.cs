faa67ffb1b0e22bea91df8dcf6251fde0d7fb3df
Xenana <k-shabatko@mail.ru>
2018-06-22 23:10:05 +0400

    Лабораторные 9 и 10 ВПФ

---
+++ AbstractOrderFood/AbstractOrderFoodViewWPF/FormChefs.xaml.cs
@@ -0,0 +1,105 @@
+﻿using AbstractOrderFoodService.ImplementationOfInter;
+using AbstractOrderFoodService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+using Unity.Attributes;
+
+namespace AbstractOrderFoodViewWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для FormChefs.xaml
+    /// </summary>
+    public partial class FormChefs : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        private readonly IChefsService service;
+
+        public FormChefs(IChefsService service)
+        {
+            InitializeComponent();
+            Loaded += FormChefs_Load;
+            this.service = service;
+        }
+
+        private void FormChefs_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<ChefsViewModel> list = service.GetList();
+                if (list != null)
+                {
+                    dataGridView.ItemsSource = list;
+                    dataGridView.Columns[0].Visibility = Visibility.Hidden;
+                    dataGridView.Columns[1].Width = DataGridLength.Auto;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var form = Container.Resolve<FormChef>();
+            if (form.ShowDialog() == true)
+                LoadData();
+        }
+
+        private void buttonUpd_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGridView.SelectedItem != null)
+            {
+                var form = Container.Resolve<FormChef>();
+                form.Id = ((ChefsViewModel)dataGridView.SelectedItem).Id;
+                if (form.ShowDialog() == true)
+                    LoadData();
+            }
+        }
+
+        private void buttonDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGridView.SelectedItem != null)
+            {
+                if (MessageBox.Show("Удалить запись?", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    int id = ((ChefsViewModel)dataGridView.SelectedItem).Id;
+                    try
+                    {
+                        service.DelElement(id);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonRef_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
